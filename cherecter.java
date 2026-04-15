e14dc67aedbaaae910feb3b5a4a337b03301cd49
Gyandeep Pal <gyandeeppal577@gmail.com>
2026-04-15 11:11:55 +0530

    hii

---
+++ cherecter.java
@@ -0,0 +1,21 @@
+public class cherecter {
+    static void subset(String p, String up){
+        //bace case
+        
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+
+        }
+        char ch = up.charAt(0);
+        //include 
+        subset(p + ch,up.substring(1));
+        //exclude
+        subset(p, up.substring(1));
+    }
+    public static void main(String[] args) {
+        // System.out.println();
+        String str = "abc";
+        subset(" ", str);
+    }
+}
